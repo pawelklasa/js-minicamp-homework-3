bedc2d046cf74c08d6ed9d0f92e38b9467c6fe25
Pav Klasa <pawel.klasa@talktalkplc.com>
2017-03-22 16:52:01 +0000

    new cat

--- exercises.js
+++ exercises.js
@@ -7,6 +7,14 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
+  var makeCat = {
+    name: name,
+    age: age,
+    meow: function() {
+      return 'Meow!';
+    }
+  };
+  return makeCat;
 }
 
 function addProperty(object, property) {
